9a29fedcbd9e385da938b1cb69b1356c2adfe17b
Vetro7 <ericvesprini@yahoo.com>
2017-05-11 04:19:51 -0400

    permine out

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -176,7 +176,7 @@ int main(int argc, char* argv[])
     command_line::add_arg(desc_cmd_sett, arg_console);
     command_line::add_arg(desc_cmd_sett, arg_testnet_on);
     command_line::add_arg(desc_cmd_sett, arg_print_genesis_tx);
-    command_line::add_arg(desc_cmd_sett, arg_genesis_block_reward_address);
+  //  command_line::add_arg(desc_cmd_sett, arg_genesis_block_reward_address);
 
     RpcServerConfig::initOptions(desc_cmd_sett);
     CoreConfig::initOptions(desc_cmd_sett);
