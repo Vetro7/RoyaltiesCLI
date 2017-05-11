ce27f875b4db7da0ab603873f6dd1e2d525a2c68
Vetro7 <ericvesprini@yahoo.com>
2017-05-10 21:15:37 -0400

    permine

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -536,7 +536,7 @@ CurrencyBuilder::CurrencyBuilder(Logging::ILogger& log) : m_currency(log) {
   blockFutureTimeLimit(parameters::CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT);
 
   moneySupply(parameters::MONEY_SUPPLY);
- //  genesisBlockReward(parameters::GENESIS_BLOCK_REWARD);
+  genesisBlockReward(parameters::GENESIS_BLOCK_REWARD);
 
   rewardBlocksWindow(parameters::CRYPTONOTE_REWARD_BLOCKS_WINDOW);
   blockGrantedFullRewardZone(parameters::CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE);
@@ -595,42 +595,42 @@ Transaction CurrencyBuilder::generateGenesisTransaction() {
   return tx;
 }
 
-// Transaction CurrencyBuilder::generateGenesisTransaction(const std::vector<AccountPublicAddress>& targets) {
-//     assert(!targets.empty());
+ Transaction CurrencyBuilder::generateGenesisTransaction(const std::vector<AccountPublicAddress>& targets) {
+     assert(!targets.empty());
  
-//     CryptoNote::Transaction tx;
-//     tx.inputs.clear();
-//     tx.outputs.clear();
-//     tx.extra.clear();
-//     tx.version = TRANSACTION_VERSION_1;
-//     tx.unlockTime = m_currency.m_minedMoneyUnlockWindow;
-//     KeyPair txkey = generateKeyPair();
-//     addTransactionPublicKeyToExtra(tx.extra, txkey.publicKey);
-//     BaseInput in;
-//     in.blockIndex = 0;
-//     tx.inputs.push_back(in);
-//     uint64_t block_reward = m_currency.m_genesisBlockReward;
-//     uint64_t target_amount = block_reward / targets.size();
-//     uint64_t first_target_amount = target_amount + block_reward % targets.size();
-//     for (size_t i = 0; i < targets.size(); ++i) {
-//       Crypto::KeyDerivation derivation = boost::value_initialized<Crypto::KeyDerivation>();
-//       Crypto::PublicKey outEphemeralPubKey = boost::value_initialized<Crypto::PublicKey>();
-//       bool r = Crypto::generate_key_derivation(targets[i].viewPublicKey, txkey.secretKey, derivation);
-//       assert(r == true);
-// //      CHECK_AND_ASSERT_MES(r, false, "while creating outs: failed to generate_key_derivation(" << targets[i].viewPublicKey << ", " << txkey.sec << ")");
-//       r = Crypto::derive_public_key(derivation, i, targets[i].spendPublicKey, outEphemeralPubKey);
-//       assert(r == true);
-//  //     CHECK_AND_ASSERT_MES(r, false, "while creating outs: failed to derive_public_key(" << derivation << ", " << i << ", " << targets[i].spendPublicKey << ")");
-//       KeyOutput tk;
-//       tk.key = outEphemeralPubKey;
-//       TransactionOutput out;
-//       out.amount = (i == 0) ? first_target_amount : target_amount;
-//       std::cout << "outs: " << std::to_string(out.amount) << std::endl;
-//       out.target = tk;
-//       tx.outputs.push_back(out);
-//     }
-//     return tx;
-// }
+     CryptoNote::Transaction tx;
+     tx.inputs.clear();
+     tx.outputs.clear();
+     tx.extra.clear();
+     tx.version = TRANSACTION_VERSION_1;
+     tx.unlockTime = m_currency.m_minedMoneyUnlockWindow;
+     KeyPair txkey = generateKeyPair();
+     addTransactionPublicKeyToExtra(tx.extra, txkey.publicKey);
+     BaseInput in;
+     in.blockIndex = 0;
+     tx.inputs.push_back(in);
+     uint64_t block_reward = m_currency.m_genesisBlockReward;
+     uint64_t target_amount = block_reward / targets.size();
+     uint64_t first_target_amount = target_amount + block_reward % targets.size();
+     for (size_t i = 0; i < targets.size(); ++i) {
+       Crypto::KeyDerivation derivation = boost::value_initialized<Crypto::KeyDerivation>();
+       Crypto::PublicKey outEphemeralPubKey = boost::value_initialized<Crypto::PublicKey>();
+       bool r = Crypto::generate_key_derivation(targets[i].viewPublicKey, txkey.secretKey, derivation);
+       assert(r == true);
+ //      CHECK_AND_ASSERT_MES(r, false, "while creating outs: failed to generate_key_derivation(" << targets[i].viewPublicKey << ", " << txkey.sec << ")");
+       r = Crypto::derive_public_key(derivation, i, targets[i].spendPublicKey, outEphemeralPubKey);
+       assert(r == true);
+  //     CHECK_AND_ASSERT_MES(r, false, "while creating outs: failed to derive_public_key(" << derivation << ", " << i << ", " << targets[i].spendPublicKey << ")");
+       KeyOutput tk;
+       tk.key = outEphemeralPubKey;
+       TransactionOutput out;
+       out.amount = (i == 0) ? first_target_amount : target_amount;
+       std::cout << "outs: " << std::to_string(out.amount) << std::endl;
+       out.target = tk;
+       tx.outputs.push_back(out);
+     }
+     return tx;
+ }
 
 CurrencyBuilder& CurrencyBuilder::numberOfDecimalPlaces(size_t val) {
   m_currency.m_numberOfDecimalPlaces = val;
