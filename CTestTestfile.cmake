963b6c9f3129930d1d30725fd71e28a65d3adf67
Vetro7 <ericvesprini@yahoo.com>
2017-07-11 03:54:47 -0400

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jamey/Documents/Coding for 3rd parties/SamSmith-EricVesprini/vetro7/Royalties
-# Build directory: /home/jamey/Documents/Coding for 3rd parties/SamSmith-EricVesprini/vetro7/Royalties
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(external)
-subdirs(src)
-subdirs(tests)
