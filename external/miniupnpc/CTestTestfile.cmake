3be7c9da633557c66496c2de9e252cd43354b2a1
Vetro7 <ericvesprini@yahoo.com>
2017-07-11 03:53:16 -0400

    Delete CTestTestfile.cmake

--- external/miniupnpc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jamey/Documents/Coding for 3rd parties/SamSmith-EricVesprini/vetro7/Royalties/external/miniupnpc
-# Build directory: /home/jamey/Documents/Coding for 3rd parties/SamSmith-EricVesprini/vetro7/Royalties/external/miniupnpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
